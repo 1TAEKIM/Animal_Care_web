7e22efae8a654bcc81c40111a2652a90ed943157
journnie <jhyun095@gmail.com>
2024-04-19 14:26:41 +0900

    settings app install

--- animal_care_web/animal_care_web/settings.py
+++ animal_care_web/animal_care_web/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
+    'community',
+    'diagnosis',
+    'mypage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
