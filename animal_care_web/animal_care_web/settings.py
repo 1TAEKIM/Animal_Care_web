6bcb9f30588b9ab757d0d46301a61d7af06e77aa
qorhundred <sunyoungfb@naver.com>
2024-04-19 15:06:38 +0900

    마이페이지 html과 css

--- animal_care_web/animal_care_web/settings.py
+++ animal_care_web/animal_care_web/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'animal_care_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'animal_care_web',
+        'USER' : 'root',
+        'PASSWORD' : 'root1234',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306'
     }
 }
 
