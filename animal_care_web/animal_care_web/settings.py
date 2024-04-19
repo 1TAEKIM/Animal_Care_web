ac889f145a3542878bef38b867b651cb9f39fb29
1TAEKIM <oneteak1@gmail.com>
2024-04-19 15:46:04 +0900

    DB setting

--- animal_care_web/animal_care_web/settings.py
+++ animal_care_web/animal_care_web/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'animal_care_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'animal_care_web',
+        'USER' : 'root',
+        'PASSWORD' : 'root1234',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
