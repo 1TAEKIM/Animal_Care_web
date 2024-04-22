e440dd9b5c69e9823147dd4c257c924748c18270
이강은 <lke7584@naver.com>
2024-04-22 12:51:14 +0900

    Add accounts func

--- animal_care_web/accounts/forms.py
+++ animal_care_web/accounts/forms.py
@@ -1,7 +1,15 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.db import models
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
+
 
 # accounts/forms.py
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = get_user_model()
+        fields = ['username']
--- animal_care_web/accounts/urls.py
+++ animal_care_web/accounts/urls.py
@@ -21,7 +21,9 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('update/', views.update, name="update"),
+    path('delete/', views.delete, name='delete'),
 ]
