0152e68d3869bdaf179b7fe99091d8075dc4fd52
이강은 <lke7584@naver.com>
2024-04-22 14:23:29 +0900

    user 모델 AUTH_USER_MODEL로 설정 변경

--- animal_care_web/mypage/models.py
+++ animal_care_web/mypage/models.py
@@ -3,11 +3,13 @@ from django.db import models
 # Create your models here.
 
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from django.conf import settings
 
 # 강아지 모델
 class Dog(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)  # 소유자
+    # owner = models.ForeignKey(User, on_delete=models.CASCADE)  # 소유자
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # 소유자
     name = models.CharField(max_length=100)  # 이름
     breed = models.CharField(max_length=100)  # 종
     age = models.IntegerField()  # 나이
