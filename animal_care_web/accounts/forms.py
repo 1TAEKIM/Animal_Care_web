22dec531745bf9ec61bfa921f23e44047734de47
이강은 <lke7584@naver.com>
2024-04-22 18:23:35 +0900

    비밀번호 수정 페이지 추가

--- animal_care_web/accounts/forms.py
+++ animal_care_web/accounts/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.db import models
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
@@ -9,7 +10,14 @@ class CustomUserCreationForm(UserCreationForm):
         model = get_user_model()
 
 
+# accounts/forms.py
 class CustomUserChangeForm(UserChangeForm):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        self.user_id = kwargs.pop('user_id', None)
+
+        super().__init__(*args, **kwargs)
+
+        self.fields['password'].help_text = "<a href='/accounts/change_password/{}'>비밀번호 변경</a>".format(self.user_id)
     class Meta(UserChangeForm.Meta):
         model = get_user_model()
         fields = ['username']
--- animal_care_web/accounts/urls.py
+++ animal_care_web/accounts/urls.py
@@ -19,11 +19,13 @@ from . import views
 
 app_name = 'accounts'
 
+# accounts/urls.py
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
-    path('update/', views.update, name="update"),
+    path('update/', views.update, name="update"),  # 회원 아이디 수정
     path('delete/', views.delete, name='delete'),
+    path('change_password/<int:user_id>', views.change_password, name='change_password')
 ]
