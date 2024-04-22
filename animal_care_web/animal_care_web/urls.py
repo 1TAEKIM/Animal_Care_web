6d0560d08533c19c640a284911cfae791931fd01
journnie <jhyun095@gmail.com>
2024-04-22 16:51:51 +0900

    board

--- animal_care_web/animal_care_web/urls.py
+++ animal_care_web/animal_care_web/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from animal_care_web import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('community', include('community.urls'))
+    path('community', include('community.urls')),
 ]
+
+# image URL
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- animal_care_web/community/urls.py
+++ animal_care_web/community/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'accounts'
+app_name = 'community'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('/', views.index, name='index'),
+    path('/board/', views.board, name='board'),
+    path('/board/<int:pk>/', views.posting, name='posting'),
+    path('/board/postwrite/', views.postwrite, name='postwrite'),
+    path('/board/<int:pk>/delete/', views.delete, name='delete'),
+    path('/board/<int:pk>/postupdate/', views.postupdate, name='postupdate'),
 ]
