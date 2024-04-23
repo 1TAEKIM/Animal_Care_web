bf915730f952e553ea264a9ff7f9990f5e643279
qorhundred <sunyoungfb@naver.com>
2024-04-23 11:51:30 +0900

    강아지 정보 없을 시 -> 정보 입력

--- animal_care_web/mypage/urls.py
+++ animal_care_web/mypage/urls.py
@@ -6,7 +6,7 @@ app_name = 'mypage'
 urlpatterns = [
     # 기존의 URL 패턴들...
     # path('', views.index, name='index'),
-    path('add_dog/<str:username>/', views.add_dog, name='add_dog'),
-    path('submit_dog_info/<str:username>/', views.dog_info_submit, name='dog_info_submit'),
+    path('add_dog/', views.add_dog, name='add_dog'),
+    path('submit_dog_info/', views.dog_info_submit, name='dog_info_submit'),
     path('<str:username>/', views.mypage_view, name='mypage_view'),  # username으로 마이페이지 URL 추가
 ]
